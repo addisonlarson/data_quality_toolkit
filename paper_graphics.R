20e20949b21001ed0b61c03ea44cb3938c6665e8
Addison Larson <39769450+addisonlarson@users.noreply.github.com>
2019-04-09 16:42:05 -0400

    clarify graphics

--- paper_graphics.R
+++ paper_graphics.R
@@ -3,14 +3,34 @@ library(tidyverse)
 library(sf)
 library(gridExtra)
 library(extrafont)
+library(tigris)
 library(viridis)
+options(tigris_class = "sf")
 loadfonts(device = "win")
 
-a <- st_read(here("dl_geo", "a_cty.shp"))
-b <- st_read(here("dl_geo", "a_puma.shp"))
-c <- st_read(here("dl_geo", "a_tad.shp"))
-d <- st_read(here("dl_geo", "a_trct.shp"))
-e <- st_read(here("dl_geo", "a_taz.shp"))
+a <- st_read(here("dl_geo", "a_cty.shp")) %>%
+  filter(str_sub(GEOID, 1, 5) == 42101)
+a_mask <- st_union(a)
+b <- st_read(here("dl_geo", "a_puma.shp")) %>%
+  st_intersection(., a_mask)
+c <- st_read(here("dl_geo", "a_tad.shp")) %>%
+  st_intersection(., a_mask)
+d <- st_read(here("dl_geo", "a_trct.shp")) %>%
+  filter(str_sub(GEOID, 1, 5) == 42101) %>%
+  mutate(destination = ifelse(GEOID == "42101000402", "Yes", "No"))
+e <- st_read(here("dl_geo", "a_taz.shp")) %>%
+  st_intersection(., a_mask)
+
+# Plot Census Tract 4.02
+ggplot(d) +
+  theme(text = element_text(family = "CMU Serif")) +
+  theme(panel.background = element_blank()) +
+  theme(legend.position = "none") +
+  scale_fill_manual(values = c("gainsboro", "#45055B")) +
+  geom_sf(aes(fill = destination), color = NA) +
+  coord_sf(datum = NA) +
+  ggtitle("Location of Census Tract 4.02")
+ggsave(here("figs", "destination.png"), width = 4.5, height = 4.5, units = "in", dpi = 400)
 
 # Plot boundaries
 a_geo <- ggplot(a) +
@@ -49,19 +69,8 @@ gridExtra::grid.arrange(a_geo, b_geo, c_geo, d_geo, e_geo, nrow = 2, ncol = 3)
 dev.off()
 
 # TAZ vs block group just for Philadelphia
-library(tigris)
-options(tigris_class = "sf")
-
-e_phila <- e %>% filter(str_sub(GEOID, 1, 5) == "42101")
-
 f_phila <- block_groups(state = 42, county = 101)
 
-e_geo_v2 <- ggplot(e_phila) +
-  theme(text = element_text(family = "CMU Serif")) +
-  theme(panel.background = element_blank()) +
-  geom_sf(color = "gray", fill = "gainsboro") +
-  coord_sf(datum = NA) +
-  ggtitle("TAZ")
 f_geo <- ggplot(f_phila) +
   theme(text = element_text(family = "CMU Serif")) +
   theme(panel.background = element_blank()) +
@@ -70,7 +79,7 @@ f_geo <- ggplot(f_phila) +
   ggtitle("Block Group")
 
 png(here("figs", "geo_taz_bg.png"), width = 7, height = 3, units = "in", res = 400)
-gridExtra::grid.arrange(e_geo_v2, f_geo, ncol = 2)
+gridExtra::grid.arrange(e_geo, f_geo, ncol = 2)
 dev.off()
 
 # Example: bicycle commuters to Census Tract 4.02
@@ -80,34 +89,54 @@ bike <- st_read(here("dl_geo", "A302103.shp")) %>%
   filter(str_sub(GEOID, 1, 5) == "42101") %>%
   select(GEOID, est, moe) %>%
   st_set_geometry(NULL)
-bike <- inner_join(d, bike)
+bike <- inner_join(d, bike) %>%
+  mutate(sub_est = ifelse(est >= 30, est, NA))
 
-png(here("figs", "bikers.png"), width = 4.5, height = 4.5, units = "in", res = 400)
+destination <- bike %>% filter(destination == "Yes")
+
 ggplot(bike) +
   theme(text = element_text(family = "CMU Serif")) +
   theme(panel.background = element_blank()) +
-  geom_sf(aes(fill = est), color = NA) +
-  scale_fill_viridis("Estimate") +
+  geom_sf(aes(fill = sub_est), color = NA) +
+  scale_fill_viridis("Estimate", na.value = "gainsboro", limits = c(0,50)) +
+  labs(title = "Estimated bicycle commuters by census tract",
+       subtitle = "Black tract is destination") +
+  geom_sf(data = destination, fill = "black", color = NA) +
   coord_sf(datum = NA) +
-  ggtitle("Estimated bicycle commuters by census tract")
-dev.off()
+ggsave(here("figs", "bikers.png"), width = 4.5, height = 4.5, units = "in", dpi = 400)
 
 # Density plots
 bike_density <- bike %>%
   st_set_geometry(NULL) %>%
-  filter(est > 30)
+  filter(est >= 30) %>%
+  arrange(est, moe)
 
 png(here("figs", "bikers_density.png"), width = 4.5, height = 4.5, units = "in", res = 400)
 ggplot(data = data.frame(x = c(-100, 200)), aes(x)) +
-  stat_function(fun = dnorm, n = 101, color = "#45055B", fill = "gainsboro",
+  stat_function(fun = dnorm, n = 101, color = "gray", fill = "gainsboro",
                 alpha = 0.5, geom = "area",
                 args = list(mean = bike_density[1,]$est, sd = bike_density[1,]$moe / 1.645)) +
-  stat_function(fun = dnorm, n = 101, color = "darkgray", fill = "gainsboro",
+  stat_function(fun = dnorm, n = 101, color = "gray", fill = "gainsboro",
                 alpha = 0.5, geom = "area",
                 args = list(mean = bike_density[2,]$est, sd = bike_density[2,]$moe / 1.645)) +
-  stat_function(fun = dnorm, n = 101, color = "#45055B", fill = "gainsboro",
+  stat_function(fun = dnorm, n = 101, color = "gray", fill = "gainsboro",
                 alpha = 0.5, geom = "area",
                 args = list(mean = bike_density[3,]$est, sd = bike_density[3,]$moe / 1.645)) +
+  stat_function(fun = dnorm, n = 101, color = "gray", fill = "gainsboro",
+                alpha = 0.5, geom = "area",
+                args = list(mean = bike_density[4,]$est, sd = bike_density[4,]$moe / 1.645)) +
+  stat_function(fun = dnorm, n = 101, color = "gray", fill = "gainsboro",
+                alpha = 0.5, geom = "area",
+                args = list(mean = bike_density[5,]$est, sd = bike_density[5,]$moe / 1.645)) +
+  stat_function(fun = dnorm, n = 101, color = "gray", fill = "gainsboro",
+                alpha = 0.5, geom = "area",
+                args = list(mean = bike_density[6,]$est, sd = bike_density[6,]$moe / 1.645)) +
+  stat_function(fun = dnorm, n = 101, color = "#45055B", fill = "gainsboro",
+                alpha = 0.5, geom = "area",
+                args = list(mean = bike_density[7,]$est, sd = bike_density[7,]$moe / 1.645)) +
+  stat_function(fun = dnorm, n = 101, color = "#45055B", fill = "gainsboro",
+                alpha = 0.5, geom = "area",
+                args = list(mean = bike_density[8,]$est, sd = bike_density[8,]$moe / 1.645)) +
   labs(x = "Potential value of estimate",
        y = "Probability",
        title = "Potential number of bikers to Census Tract 4.02") +
